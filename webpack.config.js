879ab00920d954620e3a7ca5cdf5d069b600800d
Zach Green <zach@alleyinteractive.com>
2016-08-20 20:55:24 -0400

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var postcss = require('postcss');
+var postcssCalc = require('postcss-calc');
+var postcssImport = require('postcss-import');
+var postcssNested = require('postcss-nested');
+var isHot = process.argv.indexOf('--hot') !== -1;
+
+module.exports = {
+  entry: {
+    app: 'src/app.js',
+  },
+  eslint: {
+    configFile: '.eslintrc',
+  },
+  output: {
+    path: 'build',
+    publicPath: !isHot ? '/build/' : 'http://localhost:8080/build/',
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    root: path.resolve(__dirname),
+    modulesDirectories: [
+      'node_modules'
+    ]
+  },
+  module: {
+    preloaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint',
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?modules&localIdentName=[name]__[local]__[hash:base64:5]!postcss'
+      },
+      {
+        test: /\.html$/,
+        loader: 'dom!html',
+      }
+    ]
+  },
+  postcss: function(webpack) {
+    return [
+      postcssImport({addDependencyTo: webpack}),
+      postcssNested,
+      postcssCalc,
+    ]
+  }
+};
