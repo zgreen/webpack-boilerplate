20b63e3cb67e63f22bbf9249c8070aee73440f30
Zach Green <zach@alleyinteractive.com>
2017-01-09 11:43:17 -0500

    use a .babelrc file

--- webpack.config.js
+++ webpack.config.js
@@ -47,11 +47,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015'],
-					plugins: ['transform-object-rest-spread']
-        }
+        loader: 'babel'
       },
       {
         test: /\.css$/,
